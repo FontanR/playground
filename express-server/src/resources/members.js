72eacdf2c02b97e2e0f9662c8967a2b7f54c59dd
RodrigoF <r.fontan12@gmail.com>
2023-05-11 12:18:24 -0300

    fix members

--- express-server/src/resources/members.js
+++ express-server/src/resources/members.js
@@ -24,15 +24,22 @@ router.get("/getById/:id", (req, res) => {
 
 router.post("/post", (req, res) => {
   const newMember = req.body;
-  const newMemberId = parseInt(req.body.id);
-  const foundMember = members.find((member) => member.id === newMemberId);
-  if (!newMember.email) {
-    res.status(400).json({ msg: `We need an email` });
-  } else if (foundMember) {
-    res.status(400).json({ msg: "User already exists!" });
+  if (!newMember.email || !newMember.firstName) {
+    res.status(400).json({ msg: `We need a name and email` });
   } else {
-    members.push(newMember);
-    fs.writeFile("src/data/members.json", JSON.stringify(members), (err) => {
+    const ids = members.map(user => user.id);
+    const generateId = (ids) => {
+      let newMemberId;
+      do {
+        newMemberId = Math.floor(Math.random() * 20) + 1;
+      } while (ids.includes(newMemberId));
+      return newMemberId;
+    };
+    console.log(generateId());
+    newMember.push(newMemberId);
+    console.log(newMember);
+    members.push(newMemberId);
+    fs.writeFile("src/data/members.json", JSON.stringify(members,null, 2), (err) => {
       if (err) {
         res.status(400).json({ msg: "Error! Member cant be created" });
       } else {
@@ -70,10 +77,26 @@ router.put("/put/:id", (req, res) => {
         member.firstName = updMember.firstName ? updMember.firstName : member.firstName;
         member.lastName = updMember.lastName ? updMember.lastName : member.lastName;
         member.email = updMember.email ? updMember.email : member.email;
-        res.status(200).json({ msg: `Member with id ${memberId} was updated`, member});
+        fs.writeFile("src/data/members.json", JSON.stringify(members, null, 2), (err) => {
+          if (err) {
+            res.status(400).json({ msg: "Error! User cant be updated" });
+          } else {
+            res.status(200).json({ msg: "User updated!" });
+          }
+        });
       }
-    })
+    });
   }
+});
+
+router.get("/filter/:status", (req, res) => {
+  const memberStatus = req.params.status;
+  const filteredMembers = members.filter((member) => member.status === memberStatus);
+  if(filteredMembers.length === 0) {
+    res.status(400).json({ msg: `Error! No match with member status: ${memberStatus}` });
+  } else {
+      res.status(200).json({ data: filteredMembers });
+    }
 });
 
 module.exports = router;
